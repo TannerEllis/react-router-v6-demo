7bad627a477b42ac79e52c97d47f014ae5867b2d
MyNameMatters <ellistanner.3@gmail.com>
2022-05-25 00:31:15 -0600

    added component folder and component routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import { Routes, Route, Link, Outlet } from 'react-router-dom';
+import Login from './Login';
+import Home from './Home';
+import Products from './Components/Products';
+import Search from './Components/Search';
+import ProductList from './Components/ProductList';
+import AddProduct from './Components/AddProduct';
+import DisplayProduct from './Components/DisplayProduct';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to='/add' > Add </Link>
+        <Link to='/home' > Home </Link>
+        <Link to='/products/search' > Products</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Login/>} /> 
+        <Route path='/home' element={<Home/>} /> 
+        <Route path='/products' element={<Products/>}> 
+        <Route path='search' element={<Search/>} /> 
+        <Route path='list' element={<ProductList/>} /> 
+        <Route path='add' element={<AddProduct/>} /> 
+        <Route path=':id' element={<DisplayProduct/>} />
+        </Route> 
+      </Routes>
     </div>
   );
 }
